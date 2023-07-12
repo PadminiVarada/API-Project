525d87ed81492b600693a39e95de84cb76c76d7e
PadminiVarada <paddu994@gmail.com>
2023-07-12 20:17:02 +0530

    newly added file

---
+++ requirements.js
@@ -0,0 +1,55 @@
+/* Requirements
+
+books :
+we need an API
+ a. To get all the books
+ b. To get specific book based on ISBN
+ c. To get all books based on specific category
+ d. To get all books based on specific language
+
+ authors:
+ we need an aAPI
+ a. To get all the authors
+ b. To get specific Author based on ID
+ c. TO get all authors based on books
+
+ publications:
+ a. To get all publications
+ b. to get specific publication based on ID
+ c.To get all publications based on books
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
